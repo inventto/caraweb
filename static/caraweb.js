5c2c316cb83e4c03d9eeed8cb93e8684c50a5ed5
Marlon Henrique Scalabrin <marlonscalabrin@gmail.com>
2015-06-23 16:36:00 -0300

    Adicionado TrackingJS. Melhorado segmentação do fundo.

--- static/caraweb.js
+++ static/caraweb.js
@@ -3,15 +3,20 @@
     video.height = 480;
     var canvas = document.querySelector('#canvas');
     var divCaras = document.querySelector('#corposActuales');
-    var fps = 3;
+    var fps = 1;
     var noDetectadas = 0;
     var ctx = canvas.getContext('2d');
     var mainTimer;
     var corpos = [];
     var ultimoCorpo = undefined;
+    var bgPixels = undefined;
+    var bgGray = undefined;
+    var threshold = 30;
+    var adjust = 0;
 
     var debug = document.querySelector('#debug');
     var debugBtn = document.querySelector('#debugBtn');
+    var bgBtn = document.querySelector('#bgBtn');
 
     navigator.getMedia = (navigator.getUserMedia ||
                            navigator.webkitGetUserMedia ||
@@ -33,6 +38,14 @@
         debugBtn.className = 'round success';
         debugBtn.innerHTML = 'Iniciar Debug';
       }
+    }
+
+    bgBtn.onclick = function (e) {
+      ctx.drawImage(video, 0, 0, 640, 480);
+      bg = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+      bgPixels = bg;
+      bgBlur = tracking.Image.blur(bg, canvas.width, canvas.height, 10);
+      bgGray = tracking.Image.grayscale(bgBlur, canvas.width, canvas.height, false);
     }
 
     var socket = io.connect(top.location.origin); // 'http://localhost');
@@ -60,49 +73,110 @@
     }).on('disconnect', function (data) {
       console.log("Disconnected!!!", data);
     });
-
-
-    function putImageData(ctx, imageData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight) {
-      var data = imageData.data;
-      var height = imageData.height;
-      var width = imageData.width;
-      dirtyX = dirtyX || 0;
-      dirtyY = dirtyY || 0;
-      dirtyWidth = dirtyWidth !== undefined? dirtyWidth: width;
-      dirtyHeight = dirtyHeight !== undefined? dirtyHeight: height;
-      var limitBottom = dirtyY + dirtyHeight;
-      var limitRight = dirtyX + dirtyWidth;
-      for (var y = dirtyY; y < limitBottom; y++) {
-        for (var x = dirtyX; x < limitRight; x++) {
-          var pos = y * width + x;
-          ctx.fillStyle = 'rgba(' + data[pos*4+0]
-              + ',' + data[pos*4+1]
-              + ',' + data[pos*4+2]
-              + ',' + (data[pos*4+3]/255) + ')';
-          ctx.fillRect(x + dx, y + dy, 1, 1);
-        }
+    function calcAdjust(imageLength, imageGray) {
+      adjust = 0;
+      adjustCounter = 0;
+      //*
+      for (i = 0; i < imageLength; i++) {
+        aux = imageGray[i];
+	auxBg = bgGray[i];
+	if (aux > 200 && Math.abs(aux - auxBg) < threshold) {
+		adjust += (aux - auxBg) / 1000.0;
+		adjustCounter++;
+	}
       }
+      adjust = -adjust / adjustCounter * 2000;
+      return adjust;
     }
-
     function equalize() {
+      if (!bgGray) return;
       imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      w = imageData.width;
+      h = imageData.height;
+
+      imagePixels = imageData.data;
+      imageBlur = tracking.Image.blur(imagePixels, w, h, 10);
+      imageGray = tracking.Image.grayscale(imageBlur, w, h, false); // data
+      imageLength = imageGray.length;
+
       min = 10000;
       max = 0;
-      for (i = 0; i < imageData.data.length; i+=4) {
-        aux = 0.299*imageData.data[i]+0.587*imageData.data[i+1]+0.114*imageData.data[i+2];
+      pos = 20;
+      wPos = w * pos;
+      for (i = 0; i < imageLength; i++) {
+        aux = imageGray[i];
         if (aux < min) min = aux;
-        if (aux > max) max = aux;
+	else if (aux > max) max = aux;
       }
-     
+
+      adjust = calcAdjust(imageLength, imageGray);
+      console.log("Adjust brigth defference: " + adjust);
+      //*/
+
+      for (i = 1; i < imageLength; i++) {
+        d = (imageGray[i]-min)*255/max;
+        //imageGray[i] = d;
+
+        bg = (bgGray[i]-min)*255/max;
+        diff = Math.abs(bg - d - adjust) > threshold;
+        diffR = Math.abs(bgPixels[i*4] - imagePixels[i*4]) > threshold * 2.5;
+        diffG = Math.abs(bgPixels[i*4+1] - imagePixels[i*4+1]) > threshold * 2.5;
+        diffB = Math.abs(bgPixels[i*4+2] - imagePixels[i*4+2]) > threshold * 2.5;
+        imageGray[i] = (diff || (diffR || diffG || diffB)) ? 255 : 0;
+
+      }
+      //*
+      for (i = 0; i < imageLength; i++) {
+        if (i > 2 && i < imageLength - 2 && 
+           (imageGray[i] == 255 && imageGray[i - 1] == 0 && (imageGray[i + 1] == 0 || imageGray[i + 2] == 0)) || 
+           (imageGray[i] == 255 && imageGray[i - 2] == 0 && (imageGray[i + 1] == 0 || imageGray[i + 2] == 0)) 
+        ) {
+          imageGray[i] = 0;
+        } else
+        if (i > w * 2 && i < imageLength - w * 2 && imageGray[i] == 255 && 
+           (imageGray[i - w] == 0 && (imageGray[i + w] == 0 || imageGray[i + w * 2] == 0)) ||
+           (imageGray[i - w * 2] == 0 && (imageGray[i + w] == 0 || imageGray[i + w * 2] == 0))
+        ) {
+            imageGray[i] = 0;
+        }
+        if (i > 2 && i < imageLength - 2 && 
+           (imageGray[i] == 0 && imageGray[i - 1] == 255 && (imageGray[i + 1] == 255 || imageGray[i + 2] == 255)) || 
+           (imageGray[i] == 0 && imageGray[i - 2] == 255 && (imageGray[i + 1] == 255 || imageGray[i + 2] == 255)) 
+        ) {
+            imageGray[i] = 255;
+        } else
+        if (i > w * 2 && i < imageLength - w * 2 && imageGray[i] == 0 && 
+           (imageGray[i - w] == 255 && (imageGray[i + w] == 255 || imageGray[i + w * 2] == 255)) ||
+           (imageGray[i - w * 2] == 255 && (imageGray[i + w] == 255 || imageGray[i + w * 2] == 255))
+        ) {
+         imageGray[i] = 255;
+        }
+
+      }
+      /*
+      for (i = 0; i < imageLength; i+=4) {
+        x = imageGray[i / 4] / 255.0;
+        imageData.data[i] *= x;
+        imageData.data[i+1] *= x;
+        imageData.data[i+2] *= x;
+      }
+      //*/
+      //*
       for (i = 0; i < imageData.data.length; i+=4) {
-        aux = 0.299*imageData.data[i]+0.587*imageData.data[i+1]+0.114*imageData.data[i+2];
-        aux = (aux-min)*255/max;
-        imageData.data[i] = aux;
-        imageData.data[i+1] = aux;
-        imageData.data[i+2] = aux;
-        imageData.data[i+3] = 255;
+        x = 255.0 - imageGray[i / 4];
+        imageData.data[i] += x;
+        imageData.data[i+1] += x;
+        imageData.data[i+2] += x;
       }
-      putImageData(ctx, imageData, 0, 0);
+      //*/
+      /*
+      for (i = 0; i < imageLength; i+=4) {
+        x = imageGray[i / 4];
+        imageData.data[i] = x;
+        imageData.data[i+1] = x;
+        imageData.data[i+2] = x;
+      }//*/
+      ctx.putImageData(imageData, 0, 0);
     }
 
     function captura () {      
@@ -122,12 +196,12 @@
             _corpo.getContext('2d').drawImage(canvas, corpo.x, corpo.y, corpo.width, corpo.height, 0, 0, 64, 64);
 
             // Marco de Cara
-	    if (ultimoCorpo != undefined) {
+	    /*if (ultimoCorpo != undefined) {
 		    corpo.x = (ultimoCorpo.x + corpo.x) / 2;
 		    corpo.y = (ultimoCorpo.y + corpo.y) / 2;
 		    corpo.height = (ultimoCorpo.height + corpo.height) / 2;
 		    corpo.width = (ultimoCorpo.width + corpo.width) / 2;
-	    }
+	    }*/
             ctx.beginPath();
             ctx.rect(corpo.x, corpo.y, corpo.width, corpo.height);
             ctx.fillStyle = 'rgba(46, 166, 203, 0.5)';
